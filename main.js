2bc13c26b2f92f5a76ca84d389240f022c9f29dc
João Carlos Borges <joaocarlosifsc@hotmail.com>
2023-07-26 12:05:43 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,45 @@
+const form = document.getElementById('form-contato');
+const nomes = [];
+const numero = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+})
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
+
+    if (nomes.includes(inputNomeContato.value)) {
+        alert(`O contato ${inputNomeContato.value} já foi inserido`);
+    } else {
+        nomes.push(inputNomeContato.value);
+        contato.push(parseFloat(inputNumeroContato.value));
+
+        let linha = '<tr>';
+        linha += `<td>${inputNomeContato,value}</td>`;
+        linha += `<td>${inputNumeroContato.value}</td>`;
+        linha += '</tr>';
+
+        linhas += linha;
+    }
+
+    function atualizaTabela() {
+        const corpoTabela = document.querySelector('tbody');
+        corpoTabela.innerHTML = linhas; 
+        
+    }
+
+    function atualizaContato() {
+        const contatoFinal = nomeNumeroadd();
+
+        document.getElementById('add-nome').innerHTML = addNome;
+        document.getElementById('add-numero').innerHTML = addNumero.toFixed(11);
+    }
+
+}
